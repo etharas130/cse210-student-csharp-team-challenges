8c4a6b3423ac63aa7c14ee80ea0bb807884b1ba5
Brenin Peterson <etharas130@byui.edu>
2021-10-13 11:55:08 -0600

    here we go

--- 05-jumper/Program.cs
+++ 05-jumper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05_jumper
 {
@@ -6,7 +7,25 @@ namespace _05_jumper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting point for the Jumper project.");
+            string word = "word";
+            int countDown = word.Length;
+            List<string> displayWord = new List<string>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                displayWord.Add("_ ");
+                Console.Write(displayWord[i]);
+            }
+            Console.WriteLine("");
+            Console.Write("Guess a letter [a-z]: ");
+            string guess = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                
+            }
+
+            
         }
     }
 }
