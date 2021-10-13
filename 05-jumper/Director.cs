546f05aec9c096fa4abacc852a9a84619c36646e
Collette Stapley <sta18019@byui.edu>
2021-10-13 12:37:12 -0600

    basics of director is done

--- 05-jumper/Director.cs
+++ 05-jumper/Director.cs
@@ -5,13 +5,45 @@ namespace _05_jumper
 {
     class Director
     {
+        bool _keepPlaying = true;
+        string _word = "";
+        char _guess;
+        Jumper _jumper = new Jumper();
+        WordBank _wordBank = new WordBank();
+        UserServices _userServices = new UserServices();
+        Board _board = new Board();
+
         public void StartGame()
+        {
+            SetUpGame();
+
+            while (_keepPlaying)
+            {
+                GetInputs();
+                DoUpdates();
+                DoOutputs();
+
+            }
+        }
+        // creates the word and pulls back to director. sends the word to board ot creat board, and then sends board and word to user to be displayed
+        public void SetUpGame()
         {
 
         }
+        // gets the users guess
+        public void GetInputs()
+        {
+
+        }
+        // update display
         public void DoUpdates()
         {
-            
+
+        }
+        // displays the new "board" and word status. Determines if game is over or not
+        public void DoOutputs()
+        {
+
         }
     }
 }
