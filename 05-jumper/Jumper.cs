6066b8df0542023b26c464f5f6aa52b227227084
Linda Spellman <lindaspellman@live.com>
2021-10-13 12:09:21 -0600

    fixed indent

--- 05-jumper/Jumper.cs
+++ 05-jumper/Jumper.cs
@@ -6,24 +6,24 @@ namespace _05_jumper
     class Jumper
     {
         Console.WriteLine("Starting point for the Jumper project.");
-            // Classes: Board, Director, Jumper, UserService, WordBank, Program
-            string word = "word";
-            int countDown = word.Length;
-            List<string> displayWord = new List<string>();
+        // Classes: Board, Director, Jumper, UserService, WordBank, Program
+        string word = "word";
+        int countDown = word.Length;
+        List<string> displayWord = new List<string>();
 
-            for (int i = 0; i < word.Length; i++)
-            {
-                displayWord.Add("_ ");
-                Console.Write(displayWord[i]);
-            }
-            Console.WriteLine("");
-            Console.Write("Guess a letter [a-z]: ");
-            string guess = Console.ReadLine();
+        for (int i = 0; i < word.Length; i++)
+        {
+            displayWord.Add("_ ");
+            Console.Write(displayWord[i]);
+        }
+        Console.WriteLine("");
+        Console.Write("Guess a letter [a-z]: ");
+        string guess = Console.ReadLine();
 
-            for (int i = 0; i < word.Length; i++)
-            {
-                
-            }
+        for (int i = 0; i < word.Length; i++)
+        {
+            
+        }
 
     }
 }
