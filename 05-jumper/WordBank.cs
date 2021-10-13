d41c087b17f9c21e76ee25e76c36fb921ec37cd0
Brenin Peterson <etharas130@byui.edu>
2021-10-13 12:15:47 -0600

    word bank

--- 05-jumper/WordBank.cs
+++ 05-jumper/WordBank.cs
@@ -5,6 +5,23 @@ namespace _05_jumper
 {
     class WordBank
     {
-        
+        public string _wordBank()
+        {
+            Random randomGenerator = new Random();
+            List<string> _words = new List<string>();
+
+            _words.Add("forward");
+            _words.Add("backword");
+            _words.Add("entropy");
+            _words.Add("order");
+            _words.Add("galaxy");
+            _words.Add("energy");
+
+            string word = _words[randomGenerator.Next(1, _words.Count)];
+
+            return word;
+
+        }
+
     }
 }
