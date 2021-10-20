9c20b71219fec3ec1f1e42ea6759a87fdb8ec69e
Brenin Peterson <etharas130@byui.edu>
2021-10-20 13:56:35 -0600

    basic idea

--- 06-mastermind/Compare.cs
+++ 06-mastermind/Compare.cs
@@ -3,13 +3,28 @@ using System;
 
 namespace _06_mastermind
 {
+    // This class generates a random 4 digit number 
+    // and takes the guess from the player 
+    // and compares the two adn returns a hint in the form of a string
     class Compare
     {
         int _number;
-        public void GenerateNumber()
+
+        // generates a random number 
+        private void GenerateNumber()
         {
             Random RandomGenerator = new Random();
             _number = RandomGenerator.Next(1000,10000);
+        }
+        
+        // compares the player's guess to the random number 
+        // and retnurns a complicated hint :) 
+        public string CompareGuess(string guess)
+        {
+            string hint = "";
+            string number = _number.ToString();
+
+            return hint;
         }
     }
 }
