5aa0ba63ef6925e5308d4b97c88aedb04a9eb5db
Brenin Peterson <etharas130@byui.edu>
2021-10-21 16:06:20 -0600

    compare is done :)

--- 06-mastermind/Compare.cs
+++ 06-mastermind/Compare.cs
@@ -25,11 +25,38 @@ namespace _06_mastermind
         
         // compares the player's guess to the random number 
         // and retnurns a complicated hint :) 
-        public string CompareGuess(string guess)
+        public string CompareGuess(string guess, int rNumber)
         {
-            string hint = "****";
-            string number = _number.ToString();
+            string hint = "";
+            string number = rNumber.ToString();
 
+            for (int i = 0; i < number.Length; i++)
+            {
+                if(guess[0] == number[i] && i == 0)
+                {
+                    hint += guess[0];
+                }
+                else if(guess[1] == number[i] && i == 1)
+                {
+                    hint += guess[1];
+                }
+                else if(guess[2] == number[i] && i == 2)
+                {
+                    hint += guess[2];
+                }
+                else if(guess[3] == number[i] && i == 3)
+                {
+                    hint += guess[3];
+                }
+                else if(guess[0] == number[i] || guess[1] == number[i] || guess[2] == number[i] || guess[3] == number[i])
+                {
+                    hint += "o";
+                }
+                else 
+                {
+                    hint += "x";
+                }
+            }
             return hint;
         }
     }
